860977766e80e0f2da6d1ae80cb535bb619d7bf7
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-11-14 13:29:43 +0900

    Initial Commit

---
+++ MyCameraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCameraApp
+//
+//  Created by 曽和寛貴 on 2018/11/14.
+//  Copyright © 2018 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
