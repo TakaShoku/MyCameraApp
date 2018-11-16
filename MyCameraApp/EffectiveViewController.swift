4da55228a8b6275341868f1deefab0e0c624236c
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-11-16 15:23:18 +0900

    [add] エフェクトボタンの実装

---
+++ MyCameraApp/EffectiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EffectiveViewController.swift
+//  MyCameraApp
+//
+//  Created by 曽和寛貴 on 2018/11/16.
+//  Copyright © 2018 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class EffectiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
